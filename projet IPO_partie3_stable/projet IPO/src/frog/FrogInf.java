4b088f101d6c3f4ea30214d561ba46a9abd6eae2
DouseyTB <74239997+DouseyTB@users.noreply.github.com>
2020-12-07 17:05:11 +0100

    Add files via upload

--- projet IPO_partie3_stable/projet IPO/src/frog/FrogInf.java
+++ projet IPO_partie3_stable/projet IPO/src/frog/FrogInf.java
@@ -14,7 +14,10 @@ public class FrogInf implements IFrog {
     private Element element;
     private Direction direction;
     private int distance_parcourue;   // distance parcourue par la grenouille
-    private int old_dist;             // ancienne valeur de distance_parcourue  (comme on modifie distance_parcourue dans move(), lorqu'on veut récupérer sa valeur, on obtient la valeur suivante, d'où l'interet de concerver l'ancienne valeur)
+    private int old_dist;             // ancienne valeur de distance_parcourue  (comme on modifie
+                                        // distance_parcourue dans move(), lorqu'on veut
+                                        // récupérer sa valeur, on obtient la valeur suivante, d'où
+                                        // l'intêret de conserver l'ancienne valeur)
 
     public FrogInf(Game g, int pos_x) {
         this.game = g;
@@ -46,12 +49,10 @@ public class FrogInf implements IFrog {
             new_abs = this.element.absc + 1;
         }
         else if (key == Direction.up) {
-            this.old_dist = this.distance_parcourue;
             this.distance_parcourue++;
         }
         else if (key == Direction.down && this.distance_parcourue != 0) {
             this.distance_parcourue--;
-            this.old_dist = this.distance_parcourue;
         }
 
         Element res = new Element(new_abs, this.element.ord, Color.green);
@@ -60,9 +61,6 @@ public class FrogInf implements IFrog {
 
     @Override
     public int getDist(int param) {
-        if (param == 1)
-            return this.old_dist;
-        else
             return this.distance_parcourue;
     }
 
